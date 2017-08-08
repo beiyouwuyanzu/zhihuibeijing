4b6b3f483032efeeb956970c1ba010baafb6ca72
beiyouwuyanzu <1035628914@qq.com>
2017-08-08 15:40:04 +0800

    Initial commit

---
+++ app/src/main/java/com/beiyouwuyanzu/huadongcaidan/global/GlobalConstants.java
@@ -0,0 +1,10 @@
+package com.beiyouwuyanzu.huadongcaidan.global;
+
+public class GlobalConstants {
+
+	// public static final String SERVER_URL =
+	// "http://zhihuibj.sinaapp.com/zhbj";//服务器线上前缀地址
+	public static final String SERVER_URL = "http://1743s95o31.iok.la:43185/";// 服务器主域名
+	public static final String CATEGORY_URL = SERVER_URL + "/categories.json";// 分类信息接口
+
+}                    
